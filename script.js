04e15bfce8e36d30ec236ea158142792848b049f
lilia2029 <lili.aytug2029@gbwl.org>
2026-05-06 14:21:42 +0000

    query selector!

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+//creating a boolean variable to track current mode
+let darkMode = false;
+
+/*NEW FUNCTION: called document.querySelector()
+lets us select an html ELEMENT from the page
+to store a REFRENCE (pointer) to that element
+*/
+const body = document.querySelector("body");
+//heading a JS VARIABLE that points to the element with the id="main-heading"(like in CSS)
+const heading = document.querySelector("#main-heading");
+const description = document.querySelector("#description");
+const toggleBtn = document.querySelector("#toggle");
+
+//we can change attributes & properties using JS!!
+heading.textContent = "Light & Dark Website"; //JS wrote html code!
+heading.style.color = "#ed919a";
+body.style.background = "pink";
+description.style.color = "#ed919a";
+
